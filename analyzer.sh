a148cae6253a6e23113ff1ccdf6140f71b93db36
Hms1000 <hmstakai@gmail.com>
2026-03-11 09:59:46 +0200

    Initial commit of a basic log analyzer script

---
+++ analyzer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+login_attempts=$(grep -i "failed password" /var/log/auth.log | awk "[0-9].[0-9].[0-9].[0-9]")
+
+if [ $login_attempts -gt 5 ]; then
+    echo "Failed IP Addresse(s)"
+    awk "[0-9].[0-9].[0-9].[0-9]"
+    
+    tar -czvf old_log.tar.gz auth.log
+fi
+
+git_commit() {
+    git add /var/log/auth.log
+    git commit -m "daily report commit"
+}
+
+git_commit
