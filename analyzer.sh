346dbb4f9d11b7c44a3542c8af53fd76831acb41
Hms1000 <hmstakai@gmail.com>
2026-03-21 10:24:04 +0200

    upgrades to the script

--- analyzer.sh
+++ analyzer.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 
-# log file
 LOG_FILE=/var/log/auth.log
 
-# date and time variable
 DATE=$(date +%Y-%m-%d)
 
 # list of the IP addresses that failed login
 FAILED_IPs=failed_ips_$DATE.txt
 
+MALICIOUS_IPs=malicious_ips_${DATE}.txt
+
 # ip address regex pattern
 IP_ADDRESS_REGEX=\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b
 
 
-# number of failed login attempts by each ip address
-LOGIN_ATTEMPTS=$(grep -i "failed password" $LOG_FILE | grep -oE "$IP_ADDRESS_REGEX" | sort | wc -l)
+# list of failed ip addresses
+IP_ADDRESSES=$(grep -i "failed password" $LOG_FILE | grep -oE "$IP_ADDRESS_REGEX" | sort)
 
+# write the list to a file
+echo "$IP_ADDRESSES" > failed_ips.txt
 
-# checking if the number of failed attempts is more than 5 and compressing old reports
-if [ $LOGIN_ATTEMPTS -gt 5 ]; then
-    echo "Failed IP Addresse(s)"
-    
-    grep -i "failed password" $LOG_FILE | grep -oE "$IP_ADDRESS_REGEX" | sort > failed_ips.txt
+echo "DATE - IP - OCCURENCE" > malicious.txt
 
-    cat $FAILED_IPs
+for IP in $(sort -u $FAILED_IPs);
+do count =$(grep -ci "$IP" $FAILED_IPs)
+    if [ $count -ge 5 ];then
 
-    tar -czvf $FAILED_IPs.tar.gz $FAILED_IPs
-fi
+        echo "Warning!!! Malicious IP Address: $IP: $count occurence"
+
+        echo "$DATE - $IP - $COUNT" >> malicious.txt
+
+    fi
+done
+
 
 # ensuring that each time we have a new report it is automatically commited to github
 git_commit() {
-    if [ -f $FAILED_IPs ];then
-    git add $FAILED_IPs
-    git commit -m "daily failed logins report commit"
-else
-    echo "System login attempts are less than 5"
+    if [ -f $MALICIOUS_IPs ];then
+        
+        tar -czvf $MALICIOUS_IPs.tar.gz $MALICIOUS_IPs
+        git add $MALICIOUS_IPs
+        git commit -m "daily malicious logins report commit"
+    else
+        echo "System login attempts are less than 5"
     fi
     
 }
